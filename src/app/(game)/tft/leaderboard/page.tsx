96b1ee693a0fc3df85bedb49e89f8a48ecbf1a4a
oasis345 <cogudrnr9@naver.com>
2024-04-15 18:52:12 +0900

    vercel 에러테스트

--- src/app/(game)/tft/leaderboard/page.tsx
+++ src/app/(game)/tft/leaderboard/page.tsx
@@ -1,6 +1,6 @@
 import { regions } from '../../shared/model/riot/regions';
 import { tiers } from '../model/tiers';
-import LeaderBoard from '../../shared/components/leaderboard/Leaderboard';
+import LeaderBoard from '@/app/(game)/shared/components/leaderboard/Leaderboard';
 
 export default function Page() {
   return (
